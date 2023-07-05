f842235749501d9daeff33675e10978cfcc31439
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-05 16:40:18 +0800

    chore: New arrivals init

---
+++ vite-project/src/components/NewArrivals.tsx
@@ -0,0 +1,10 @@
+const NewArrivals = () => {
+  return (
+    <section className="px-5 mt-8 flex items-center">
+      <h1 className="font-bold text-5xl pl-10">New Arrivals</h1>
+      <div className="bg-black h-2 w-full"></div>
+    </section>
+  );
+};
+
+export default NewArrivals;
