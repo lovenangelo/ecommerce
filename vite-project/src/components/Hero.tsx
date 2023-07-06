d88773349bb570232213b0b7effd480f56cc5648
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-06 11:20:52 +0800

    refactor: ui bug in hero width size

--- vite-project/src/components/Hero.tsx
+++ vite-project/src/components/Hero.tsx
@@ -1,23 +1,23 @@
-import heroBg2 from "@/assets/images/hero-bg-2.jpg";
 import { Button } from "./ui/button";
 import icons from "@/lib/icons";
+import images from "@/lib/images";
 
 const Hero = () => {
   return (
-    <section className="h-96 w-screen px-5">
+    <section className="h-96 w-full">
       <div className="flex justify-end relative">
-        <div className="z-20 absolute bg-[#DEDEDE]/50 w-96 h-96 rounded-md items-start justify-center flex flex-col p-8 space-y-4">
+        <div className="z-20 absolute bg-[#DEDEDE]/50 w-96 h-96 rounded-r-md items-start justify-center flex flex-col p-8 space-y-4">
           <h1 className="z-30 text-5xl font-bold"> Shop in style!</h1>
           <p>Classy and trendy collections.</p>
           <Button className="space-x-2">
-            <icons.moveRight />
+            <icons.arrowRight />
             <span>See more</span>
           </Button>
         </div>
       </div>
       <img
         className="h-full w-full rounded-md object-cover"
-        src={heroBg2}
+        src={images.heroBg2}
         alt="hero backround"
       />
     </section>
