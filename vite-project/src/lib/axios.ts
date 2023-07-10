4c21942933426b19b8a3f0f32fd9a9848f70b9b4
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-10 12:53:52 +0800

    fix: csrf mismatch issue solved

--- vite-project/src/lib/axios.ts
+++ vite-project/src/lib/axios.ts
@@ -5,7 +5,25 @@ const axiosClient = axios.create({
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "X-CSRF-TOKEN": getCsrfTokenFromCookie(), // get the CSRF token from a cookie
   },
+  withCredentials: true,
 });
+
+function getCsrfTokenFromCookie() {
+  const name = "XSRF-TOKEN=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(";");
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+}
 
 export default axiosClient;
