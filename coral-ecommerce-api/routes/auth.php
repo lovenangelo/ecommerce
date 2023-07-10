a39124180b95cec01fb9208dbf5d8b7a0175ad4e
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-10 16:56:05 +0800

    refactor: rollback to defaults

--- coral-ecommerce-api/routes/auth.php
+++ coral-ecommerce-api/routes/auth.php
@@ -9,11 +9,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-  // ->middleware('guest')
+  ->middleware('guest')
   ->name('register');
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-  // ->middleware('guest')
+  ->middleware('guest')
   ->name('login');
 
 Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
