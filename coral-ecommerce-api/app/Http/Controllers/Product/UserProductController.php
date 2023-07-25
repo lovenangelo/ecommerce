c27ee93115a664b143a94b42205b9648d6c1a658
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-25 15:54:13 +0800

    feat: product update api

--- coral-ecommerce-api/app/Http/Controllers/Product/UserProductController.php
+++ coral-ecommerce-api/app/Http/Controllers/Product/UserProductController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product\Product;
+use App\Models\Product\ProductImage;
 use App\Models\User;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UserProductController extends Controller
 {
@@ -31,9 +33,41 @@ class UserProductController extends Controller
    */
   public function update(Request $request, $id)
   {
+    $validated = $request->validate(
+      [
+        'name' => 'required|string',
+        'description' => 'required|string',
+        'price' => 'required|numeric',
+        'quantity' => 'required|integer',
+        'category' => 'required|string',
+        'sizes' => 'required|string',
+        'color' => 'required|string',
+        'payment_options' => 'required|string',
+        'subtitle' => 'required|string',
+        'brand' => 'required|string',
+      ]
+    );
     $product = Product::findOrFail($id);
     $product->fill($request->all());
     $product->save();
+
+    clock($request->file('image'));
+    if ($request->file('image') !== null) {
+
+      // Delete the old file if it exists
+      $oldImage = $product->image->url;
+      Storage::delete(basename($oldImage));
+
+      // Store the new file with the specified name in the products/category folder
+      $path = $request->file('image')->store("products/{$validated['category']}");
+      $image = $product->image;
+      $productImage = ProductImage::findOrFail($image->id);
+
+      // ProductImage::create(['product_id' => $product->id, 'url' => $path]);
+      $productImage->fill(['url' => $path]);
+      $productImage->save();
+    }
+
 
     return response()->noContent();
   }
