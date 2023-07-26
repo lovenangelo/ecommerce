c07da7091a7908431e7917aa074c85acb70235f5
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-26 11:49:08 +0800

    refactor: cart loading skeleton

--- vite-project/src/components/Cart/SkeletonLoading.tsx
+++ vite-project/src/components/Cart/SkeletonLoading.tsx
@@ -1,5 +1,16 @@
+import { Skeleton } from "../ui/skeleton";
+
 const SkeletonLoading = () => {
-  return <div>SkeletonLoading</div>;
+  return (
+    <div className="max-h-[400px] h-max grid grid-cols-3 mt-4 gap-8 ">
+      <div className="col-span-2 w-full min-h-[400px] h-max row-auto overflow-auto rounded-lg border bg-gray-100">
+        <Skeleton className="w-full h-[400px]" />
+      </div>
+      <div className="col-span-1 h-full bg-gray-100 rounded-lg border">
+        <Skeleton className="h-full w-full" />
+      </div>
+    </div>
+  );
 };
 
 export default SkeletonLoading;
