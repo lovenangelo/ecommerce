c3398940977087b2900567eff68ed6656fe16d4a
Loven Angelo <lovenangelo.dev@gmail.com>
2023-07-31 10:11:29 +0800

    chore: state order payment method

--- vite-project/src/components/Cart/Checkout/components/SelectPaymentMethod.tsx
+++ vite-project/src/components/Cart/Checkout/components/SelectPaymentMethod.tsx
@@ -2,38 +2,40 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import Icons from "@/lib/icons";
 import { useState } from "react";
-type PaymentMethod = "CREDIT-CARD" | "COD";
+type PaymentMethod = "card" | "cod";
 import BraintreeDropIn from "./BrainTreeDropIn";
 import { Button } from "@/components/ui/button";
+import { useAppDispatch } from "@/redux/hooks";
+import { updatePaymentMethod } from "@/redux/slices/orderPaymentMethodSlice";
 
 const SelectPaymentMethod = () => {
-  const [paymentMethod, setPaymentMethod] =
-    useState<PaymentMethod>("CREDIT-CARD");
-
+  const [paymentMethod, setPaymentMethod] = useState<PaymentMethod>("card");
+  const dispatch = useAppDispatch();
   return (
     <RadioGroup
       className="my-8"
-      onValueChange={(val) => {
-        if (val == "CREDIT-CARD" || val == "COD") setPaymentMethod(val);
+      onValueChange={(val: "card" | "cod") => {
+        if (val == "card" || val == "cod") setPaymentMethod(val);
+        dispatch(updatePaymentMethod(val));
       }}
-      defaultValue="CREDIT-CARD"
+      defaultValue="card"
     >
       {" "}
       <div className="flex items-center space-x-2">
-        <RadioGroupItem value="CREDIT-CARD" id="CREDIT-CARD" />
-        <Label className="flex items-center" htmlFor="CREDIT-CARD">
+        <RadioGroupItem value="card" id="card" />
+        <Label className="flex items-center" htmlFor="card">
           Credit/Debit Card{" "}
           <span className="ml-2">
             <Icons.creditCard />
           </span>
         </Label>
       </div>{" "}
-      {paymentMethod == "CREDIT-CARD" && <BraintreeDropIn showDropIn={true} />}{" "}
+      {paymentMethod == "card" && <BraintreeDropIn showDropIn={true} />}{" "}
       <div className="flex items-center space-x-2">
-        <RadioGroupItem value="COD" id="COD" />
-        <Label htmlFor="COD">Cash On Delivery (COD)</Label>
+        <RadioGroupItem value="cod" id="cod" />
+        <Label htmlFor="cod">Cash On Delivery (COD)</Label>
       </div>
-      {paymentMethod == "COD" && (
+      {paymentMethod == "cod" && (
         <Button className="w-max mt-8">Place Order</Button>
       )}
     </RadioGroup>
