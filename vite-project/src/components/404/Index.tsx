07f5fbef3df90bcaa400cb0f9aae23a0c2539369
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-01 13:36:24 +0800

    refactor: responsive 404 text

--- vite-project/src/components/404/Index.tsx
+++ vite-project/src/components/404/Index.tsx
@@ -1,7 +1,9 @@
 const Index = () => {
   return (
     <div className="h-[calc(100vh-80px)] w-full flex justify-center items-center">
-      <h1 className="font-bold text-5xl text-[#17494D]">404 NOT FOUND</h1>
+      <h1 className="font-bold text-2xl md:text-5xl text-[#17494D]">
+        404 NOT FOUND
+      </h1>
     </div>
   );
 };
