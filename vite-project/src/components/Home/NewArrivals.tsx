4a259a346bb8fd620be2b8a3e743c709c3660f4b
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-01 14:56:44 +0800

    refactor: landing page spacing and gaps between components

--- vite-project/src/components/Home/NewArrivals.tsx
+++ vite-project/src/components/Home/NewArrivals.tsx
@@ -33,16 +33,16 @@ const NewArrivals = () => {
   });
 
   return (
-    <section className="container w-full mt-8 md:mt-12 grid grid-cols-1 rows-auto md:grid-cols-6 md:grid-row-2 min-h-screen">
-      <div className="flex justify-start items-start col-span-2 h-max">
-        <div className="flex items-center justify-start h-max">
+    <section className="container w-full mt-8 lg:mt-12 grid grid-cols-1 rows-auto md:grid-cols-6 lg:grid-row-2 lg:min-h-screen">
+      <div className="justify-start items-start col-span-2 h-max">
+        <div className="flex items-center justify-start lg:h-max">
           <icons.arrowDown className="h-36 w-36 md:h-24 md:w-24" />
           <h1 className="font-bold text-5xl">New Arrivals</h1>
         </div>
       </div>
       <div className="col-span-4 bg-primary h-2 w-full mt-12 hidden md:block" />
       <div className="col-span-6">
-        <div className="grid grid-cols-2 rows-auto md:grid-cols-4 rows-auto gap-4 max-w-full mt-12 md:mt-4">
+        <div className="grid grid-cols-2 rows-auto md:grid-cols-4 rows-auto gap-4 max-w-full mt-12 lg:mt-4">
           {products}
         </div>
       </div>
