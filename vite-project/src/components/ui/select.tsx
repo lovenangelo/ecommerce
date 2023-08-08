4f3665883a13442ece4a0ed0e4be4594fc34275d
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-08 16:28:57 +0800

    chore: removed the check in shadcn select

--- vite-project/src/components/ui/select.tsx
+++ vite-project/src/components/ui/select.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CheckIcon } from "@radix-ui/react-icons";
+// import { CheckIcon } from "@radix-ui/react-icons";
 import * as SelectPrimitive from "@radix-ui/react-select";
 
 import { cn } from "@/lib/utils";
@@ -83,7 +83,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <CheckIcon className="h-4 w-4" />
+        {/* <CheckIcon className="h-4 w-4" /> */}
       </SelectPrimitive.ItemIndicator>
     </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
