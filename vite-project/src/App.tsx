fab7b4973fa060e47ee3d04e30e1df01006f6e5b
Loven Angelo <lovenangelo.dev@gmail.com>
2023-08-09 10:51:44 +0800

    refactor: handleAuthState

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -23,13 +23,20 @@ const LazyFooter = lazy(() => import("@/components/Home/Footer"));
 function App() {
   const user = useAppSelector((state) => state.user.value);
   const dispatch = useAppDispatch();
+
   const handleAuthState = async () => {
-    if (user && !user.remember_me) {
-      dispatch(removeUser());
-      await persistor.purge();
-      await axiosClient.get("/sanctum/csrf-cookie");
-      await axiosClient.post("/logout");
-    }
+    await axiosClient
+      .get("/sanctum/csrf-cookie")
+      .then(async () => {
+        if (user && !user.remember_me) {
+          dispatch(removeUser());
+          await persistor.purge();
+          await axiosClient.post("/logout");
+        }
+      })
+      .catch(async () => {
+        await axiosClient.post("/logout");
+      });
   };
 
   useBeforeunload(handleAuthState);
